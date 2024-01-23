387ff22b3012899a4a80c38b0b2e4e4f84987b55
gabriel31dias <t.gabrieldias.dev@engefoto.com.br>
2024-01-23 18:52:25 -0300

    adjustments

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -13,28 +13,19 @@ class MainsController < ApplicationController
 
   def home
     # Seu código para gerar a string JSON
-@banners = Banner.where(active: true).select do |banner|
-  banner.image.attached?
-end.map do |banner|
-  {
-    id: banner.id,
-    name: banner.name,
-    description: banner.description,
-    active: banner.active,
-    image_url: url_for(banner.image)
-  }
-end
+    @banners = Banner.where(active: true).select do |banner|
+      banner.image.attached?
+    end.map do |banner|
+      {
+        id: banner.id,
+        name: banner.name,
+        description: banner.description,
+        active: banner.active,
+        image_url: url_for(banner.image)
+      }
+    end
 
-# Remova a linha que está transformando @banners em uma lista de image_url
-# @banners = @banners.pluck(:image_url)
-
-# Converta para JSON
-@banners_json = @banners.to_json(json_options: { escape: true })
-
-# Converta a string JSON para base64
-@banners_base64 = Base64.strict_encode64(@banners_json)
-
-
-    
+    @banners_json = @banners.to_json(json_options: { escape: true })
+    @banners_base64 = Base64.strict_encode64(@banners_json)
   end
 end
